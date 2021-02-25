eec0133117de8d3bae33f51868f859fb4ada005d
Raufo Jr <raufo.rastrero@hotmail.com>
2021-02-24 21:14:13 -0300

    Terceiro  dia do NLW4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+
+import { ChallengeBox } from '../components/ChallengeBox';
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from "../components/ExperienceBar";
@@ -22,9 +24,10 @@ export default function Home() {
           <Profile />
           <CompletedChallenges />
           <Countdown />
-          <div>
 
-          </div>
+        </div>
+        <div>
+          <ChallengeBox />
         </div>
 
       </section>
